1d4399d51cf2a1ae7761855c331860e03a3d59b7
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2010-11-02 14:15:51 +0100

    initial commit

---
+++ src/main/java/de/nosebrain/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package de.nosebrain.common.exception;
+
+/**
+ * the resource wasn't found
+ * @author nosebrain
+ */
+public class ResourceNotFoundException extends Exception {
+	private static final long serialVersionUID = -1712728448471811000L;
+
+}
---
+++ src/main/java/de/nosebrain/common/exception/ValidationException.java
@@ -0,0 +1,10 @@
+package de.nosebrain.common.exception;
+
+/**
+ * the resource contains ConstraintViolation 
+ * @author nosebrain
+ */
+public class ValidationException extends Exception {
+	private static final long serialVersionUID = 4244611242278763339L;
+
+}
