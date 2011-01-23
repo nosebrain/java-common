a696def5537f3cc4446bf196f6f07d23791ba144
Daniel Zoller <nosebrain@gmx.net>
2011-01-23 21:02:38 +0100

    added authentication exception

---
+++ src/main/java/de/nosebrain/common/exception/AuthenticationException.java
@@ -0,0 +1,9 @@
+package de.nosebrain.common.exception;
+
+/**
+ * @author nosebrain
+ */
+public class AuthenticationException extends Exception {
+	private static final long serialVersionUID = -8130962097168114815L;
+
+}
