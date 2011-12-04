382cfaf6c77534b3a71f226b4546dd487efab8a7
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2011-12-04 22:16:53 +0100

    convertToUrl now static

--- src/main/java/de/nosebrain/mybatis/typehandler/URLTypeHandler.java
+++ src/main/java/de/nosebrain/mybatis/typehandler/URLTypeHandler.java
@@ -16,29 +16,29 @@ import org.apache.ibatis.type.TypeHandler;
  */
 public class URLTypeHandler implements TypeHandler {
 
-	@Override
-	public void setParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) throws SQLException {
-		final URL url = (URL) parameter;
-		ps.setString(i, url.toString());
-	}
-
-	@Override
-	public Object getResult(ResultSet rs, String columnName) throws SQLException {
-		return this.convertToURL(rs.getString(columnName));
-	}
-	
-	private URL convertToURL(final String string) {
+	private static URL convertToURL(final String string) {
 		try {
 			return new URL(string);
-		} catch (MalformedURLException e) {
+		} catch (final MalformedURLException e) {
 			// TODO: log
 			return null;
 		}
 	}
 
 	@Override
-	public Object getResult(CallableStatement cs, int columnIndex) throws SQLException {
-		return this.convertToURL(cs.getString(columnIndex));
+	public void setParameter(final PreparedStatement ps, final int i, final Object parameter, final JdbcType jdbcType) throws SQLException {
+		final URL url = (URL) parameter;
+		ps.setString(i, url.toString());
+	}
+
+	@Override
+	public Object getResult(final ResultSet rs, final String columnName) throws SQLException {
+		return convertToURL(rs.getString(columnName));
+	}
+
+	@Override
+	public Object getResult(final CallableStatement cs, final int columnIndex) throws SQLException {
+		return convertToURL(cs.getString(columnIndex));
 	}
 
 }
