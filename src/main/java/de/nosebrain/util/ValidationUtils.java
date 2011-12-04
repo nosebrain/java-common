b43bfb6c5dbea9c1adf2f25725c0a2cdb2a92398
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2011-12-04 22:18:55 +0100

    added persent method for collection

--- src/main/java/de/nosebrain/util/ValidationUtils.java
+++ src/main/java/de/nosebrain/util/ValidationUtils.java
@@ -1,12 +1,14 @@
 package de.nosebrain.util;
 
+import java.util.Collection;
+
 /**
  * 
  * @author nosebrain
  */
 public final class ValidationUtils {
 	private ValidationUtils() { }
-	
+
 	/**
 	 * @param object the object to check
 	 * @return <code>true</code> if object is not null
@@ -14,12 +16,20 @@ public final class ValidationUtils {
 	public static boolean present(final Object object) {
 		return object != null;
 	}
-	
+
 	/**
 	 * @param sequence the sequence to check
 	 * @return <code>true</code> if seqenence is not null and has a length
 	 */
-	public static boolean present(CharSequence sequence) {
+	public static boolean present(final CharSequence sequence) {
 		return sequence != null && sequence.toString().trim().length() > 0;
+	}
+
+	/**
+	 * @param collection the collection to check
+	 * @return <code>true</code> iff collection is not null and is not empty
+	 */
+	public static boolean present(final Collection<?> collection) {
+		return collection != null && !collection.isEmpty();
 	}
 }
