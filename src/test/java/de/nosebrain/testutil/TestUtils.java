5a4c24958765b065fb8c1b2aef35000747ed501f
Daniel Zoller <nosebrain@gmx.net>
2012-06-22 13:06:06 +0200

    fixed tests by disableing TestUtils

--- src/test/java/de/nosebrain/testutil/TestUtils.java
+++ src/test/java/de/nosebrain/testutil/TestUtils.java
@@ -3,9 +3,21 @@ package de.nosebrain.testutil;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.junit.Ignore;
+
+/**
+ * class for common test methods
+ * 
+ * @author nosebrain
+ */
+@Ignore
 public final class TestUtils {
 	private TestUtils() {}
 
+	/**
+	 * @param url the url to convert
+	 * @return the url
+	 */
 	public static URL parseURL(final String url) {
 		try {
 			return new URL(url);
