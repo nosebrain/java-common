8ea6d22147a7aff26567c4b75b89fbd03912e8d0
Daniel Zoller <nosebrain@gmx.net>
2013-12-29 21:56:30 +0100

    updated dependency versions and plugins
    update java from 6 to 7

--- src/main/java/de/nosebrain/mybatis/typehandler/LocaleTypeHandler.java
+++ src/main/java/de/nosebrain/mybatis/typehandler/LocaleTypeHandler.java
@@ -13,14 +13,14 @@ import org.apache.ibatis.type.TypeHandler;
  * {@link TypeHandler} for {@link Locale}
  * @author nosebrain
  */
-public class LocaleTypeHandler implements TypeHandler {
+public class LocaleTypeHandler implements TypeHandler<Locale> {
 
 	private static final Locale convertLocale(final String string) {
 		return new Locale(string);
 	}
 
 	@Override
-	public void setParameter(final PreparedStatement ps, final int i, final Object parameter, final JdbcType jdbcType) throws SQLException {
+	public void setParameter(final PreparedStatement ps, final int i, final Locale parameter, final JdbcType jdbcType) throws SQLException {
 		if (parameter instanceof Locale) {
 			final Locale locale = (Locale) parameter;
 			ps.setString(i, locale.toString());
@@ -28,13 +28,18 @@ public class LocaleTypeHandler implements TypeHandler {
 	}
 
 	@Override
-	public Object getResult(final ResultSet rs, final String columnName) throws SQLException {
+	public Locale getResult(final ResultSet rs, final String columnName) throws SQLException {
 		return convertLocale(rs.getString(columnName));
 	}
 
 	@Override
-	public Object getResult(final CallableStatement cs, final int columnIndex) throws SQLException {
+	public Locale getResult(final CallableStatement cs, final int columnIndex) throws SQLException {
 		return convertLocale(cs.getString(columnIndex));
+	}
+	
+	@Override
+	public Locale getResult(final ResultSet rs, final int columnIndex) throws SQLException {
+		return convertLocale(rs.getString(columnIndex));
 	}
 
 }
