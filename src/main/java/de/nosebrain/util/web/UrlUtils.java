dc53c423ec3e8b1208c1707179838dffaf013c53
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2020-02-15 16:51:37 +0100

    added encode help function

--- src/main/java/de/nosebrain/util/web/UrlUtils.java
+++ src/main/java/de/nosebrain/util/web/UrlUtils.java
@@ -2,12 +2,21 @@ package de.nosebrain.util.web;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 public class UrlUtils {
 
   public static String decodeUrlString(final String urlString) {
     try {
       return URLDecoder.decode(urlString, "UTF-8");
+    } catch (final UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static String encodeUrlString(final String urlString) {
+    try {
+      return URLEncoder.encode(urlString, "UTF-8");
     } catch (final UnsupportedEncodingException e) {
       throw new RuntimeException(e);
     }
